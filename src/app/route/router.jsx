890bb9faedc566c2bef9c8c2f608c8c247e64e60
seojinwan <jinwanseo@gmail.com>
2023-04-01 17:34:31 +0900

    add redirects;

--- src/app/route/router.jsx
+++ src/app/route/router.jsx
@@ -3,18 +3,15 @@ import SpeechComponent from "views/Speech";
 
 const { createBrowserRouter } = require("react-router-dom");
 
-export default createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          path: "speech",
-          element: <SpeechComponent />,
-        },
-      ],
-    },
-  ],
-  { basename: process.env.PUBLIC_URL }
-);
+export default createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "speech",
+        element: <SpeechComponent />,
+      },
+    ],
+  },
+]);
